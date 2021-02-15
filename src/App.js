46baa3853807b6abdd8eba2a92ab79fbb31720af
Dhananjaya Poojari <DPoojari@magsw.com>
2021-02-15 19:31:31 +0530

    list all the data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 import Header from "./components/Header";
 import WeatherSearch from "./components/SearchLocation";
 import SearchResult from './components/SearchResult'
@@ -7,6 +7,8 @@ require("dotenv").config();
 
 function App() {
   const [searchData, setSearchData] = useState({});
+  const wrapperRef = useRef(null);
+
   const fetchChargingLocation = async (lat, lon) => {
     const url = `https://api.openchargemap.io/v3/poi/?output=json&maxresults=10&compact=true&verbose=false&latitude=${lat}&longitude=${lon}&distanceunit=KM`;
     const res = await fetch(url);
@@ -25,9 +27,9 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" ref={wrapperRef}>
       <Header className="App-header"></Header>
-      <WeatherSearch api_call={fetchLongitudeAndLatitude} />
+      <WeatherSearch api_call={fetchLongitudeAndLatitude}  wrapperRef={wrapperRef}/>
       { searchData.length>0 && <SearchResult searchData={searchData}/>}
     </div>
   );
--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -1,21 +1,76 @@
 import React from "react";
 import { Button, TextField, Grid } from "@material-ui/core";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+let timer;
 
-function WeatherSearch({ api_call }) {
+function WeatherSearch({ api_call, wrapperRef }) {
   const [location, setLocation] = useState("");
+
   const handleOnChange = (event) => {
     setLocation(event.target.value);
+    timer && clearTimeout(timer);
+    timer = setTimeout(async function () {
+      const url = `https://us1.locationiq.com/v1/search.php?key=pk.b85addb584eaefbe9e93b63a33a9a3e9&q=${event.target.value}&countrycodes=in&format=json&limit=10`;
+      const res = await fetch(url);
+      const data = await res.json();
+      const pokemon = data;
+      setOptions(pokemon);
+      setDisplay(true);
+    }, 500);
   };
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const updatePokeDex = (poke) => {
+    setLocation(poke);
+    document.getElementById("Location").value = poke;
+    setOptions([]);
+  };
+
+  const handleClickOutside = (event) => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
   return (
     <div>
       <Grid container direction="column" justify="center" alignItems="center">
-        <TextField id="Location" label="Location" onChange={handleOnChange} style = {{width: 600,height: 40,marginTop:10 }} variant="filled"  />
+        <TextField
+          id="Location"
+          label="Location"
+          onChange={handleOnChange}
+          style={{ width: 600, height: 40, marginTop: 10, marginBottom: 10 }}
+          variant="filled"
+        />
+        {options.length > 0 && (
+          <div className="autoContainer">
+            {options.map((value, i) => {
+              return (
+                <div
+                  onClick={() => updatePokeDex(value.display_name)}
+                  className="option"
+                  key={i}
+                  tabIndex="0"
+                >
+                  <span>{value.display_name}</span>
+                </div>
+              );
+            })}
+          </div>
+        )}
         <br />
         <Button
           variant="contained"
           color="primary"
-          style={{marginBottom:10,marginTop:10}}
+          style={{ marginBottom: 10, marginTop: 10 }}
           onClick={() => api_call(location)}
         >
           Search

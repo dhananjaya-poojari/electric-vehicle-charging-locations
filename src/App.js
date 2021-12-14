61633bc9325f69bfa5360b85bca8d1d626fa0680
Dhananjaya Poojari <DPoojari@magsw.com>
2021-12-14 18:22:31 +0530

    API key updated

--- src/App.js
+++ src/App.js
@@ -6,31 +6,34 @@ import SearchResult from './components/SearchResult'
 require("dotenv").config();
 
 function App() {
-  const [searchData, setSearchData] = useState({});
-  const fetchChargingLocation = async (lat, lon) => {
-    const url = `https://api.openchargemap.io/v3/poi/?output=json&maxresults=10&compact=true&verbose=false&latitude=${lat}&longitude=${lon}&distanceunit=KM`;
-    const res = await fetch(url);
-    const data = await res.json();
-    setSearchData(data);
-  };
+    const [searchData, setSearchData] = useState({});
+    const fetchChargingLocation = async(lat, lon) => {
+        const url = `https://api.openchargemap.io/v3/poi/?key=c31315d8-f49c-4610-88c8-76cd007888bc&output=json&maxresults=10&compact=true&verbose=false&latitude=${lat}&longitude=${lon}&distanceunit=KM`;
+        const res = await fetch(url);
+        const data = await res.json();
+        setSearchData(data);
+    };
 
-  const fetchLongitudeAndLatitude = async (location) => {
-    if (location) {
-      const API_key = process.env.API_KEY;
-      const url = `https://us1.locationiq.com/v1/search.php?key=pk.b85addb584eaefbe9e93b63a33a9a3e9&q=${location}&countrycodes=in&format=json&limit=1`;
-      const res = await fetch(url);
-      const data = await res.json();
-      await fetchChargingLocation(data[0].lat, data[0].lon);
+    const fetchLongitudeAndLatitude = async(location) => {
+        if (location) {
+            const API_key = process.env.API_KEY;
+            const url = `https://us1.locationiq.com/v1/search.php?key=pk.b85addb584eaefbe9e93b63a33a9a3e9&q=${location}&countrycodes=in&format=json&limit=1`;
+            const res = await fetch(url);
+            const data = await res.json();
+            await fetchChargingLocation(data[0].lat, data[0].lon);
+        }
+    };
+
+    return ( <
+        div className = "App" >
+        <
+        Header className = "App-header" > < /Header> <
+        WeatherSearch api_call = { fetchLongitudeAndLatitude }
+        /> {
+            searchData.length > 0 && < SearchResult searchData = { searchData }
+            />} <
+            /div>
+        );
     }
-  };
 
-  return (
-    <div className="App">
-      <Header className="App-header"></Header>
-      <WeatherSearch api_call={fetchLongitudeAndLatitude} />
-      { searchData.length>0 && <SearchResult searchData={searchData}/>}
-    </div>
-  );
-}
-
-export default App;
+    export default App;
